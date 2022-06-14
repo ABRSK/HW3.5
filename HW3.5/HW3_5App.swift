509801ea181a93254168920b9aa9d891e5713175
AreyBRSK <areybrsk@gmail.com>
2022-06-14 11:35:42 +0300

    Initial Commit

---
+++ HW3.5/HW3_5App.swift
@@ -0,0 +1,17 @@
+//
+//  HW3_5App.swift
+//  HW3.5
+//
+//  Created by Андрей Барсук on 14.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct HW3_5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
